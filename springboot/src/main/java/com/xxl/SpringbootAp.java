d5013cb0912e3e9a5b0115870179c96a9daf5215
web_xl <597146220@qq.com>
2019-12-30 23:40:11 +0800

    springboot 学习

---
+++ springboot/src/main/java/com/xxl/SpringbootAp.java
@@ -0,0 +1,18 @@
+package com.xxl;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 标注一个主程序类
+ */
+@SpringBootApplication
+public class SpringbootAp {
+    public static void main(String[] args) {
+
+        //将spring 应用跑起来
+        SpringApplication.run(SpringbootAp.class,args);
+    }
+}
